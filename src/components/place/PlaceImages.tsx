58d4ccb09374a7c1a1c4762c6f1d4d4ccf8b739f
최준성 <147041315+yumXD@users.noreply.github.com>
2024-08-12 01:36:18 +0900

    Design: 상세페이지 이미지들 확대하여 원본사진 보기 디자인
    모달창

--- src/components/place/PlaceImages.tsx
+++ src/components/place/PlaceImages.tsx
@@ -5,8 +5,10 @@ interface PlaceImagesProps {
     images: string[];
 }
 
-function PlaceImages({ images }: PlaceImagesProps) {
+
+function PlaceImages({images}: PlaceImagesProps) {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const prevSlide = () => {
         setCurrentIndex((prevIndex) =>
@@ -20,36 +22,57 @@ function PlaceImages({ images }: PlaceImagesProps) {
         );
     };
 
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
-        <div className="slider">
-            <button className="slider-button prev-button" onClick={prevSlide}>
-                &#10094;
-            </button>
-            <div className="slider-images">
-                {images.map((image, index) => (
+        <div>
+            <div className="slider">
+                <button className="slider-button prev-button" onClick={prevSlide}>
+                    &#10094;
+                </button>
+                <div className="slider-images" onClick={openModal}>
+                    {images.map((image, index) => (
+                        <img
+                            key={index}
+                            src={image}
+                            alt={`Slide ${index}`}
+                            className={`slider-image ${
+                                index === currentIndex ? 'active' : ''
+                            }`}
+                        />
+                    ))}
+                </div>
+                <button className="slider-button next-button" onClick={nextSlide}>
+                    &#10095;
+                </button>
+                <div className="dots-container">
+                    {images.map((_, index) => (
+                        <span
+                            key={index}
+                            className={`dot ${
+                                index === currentIndex ? 'active' : ''
+                            }`}
+                        ></span>
+                    ))}
+                </div>
+            </div>
+
+            {isModalOpen && (
+                <div className="modal" onClick={closeModal}>
+                    <span className="close-modal">&times;</span>
                     <img
-                        key={index}
-                        src={image}
-                        alt={`Slide ${index}`}
-                        className={`slider-image ${
-                            index === currentIndex ? 'active' : ''
-                        }`}
+                        className="modal-content"
+                        src={images[currentIndex]}
+                        alt={`Original Slide ${currentIndex}`}
                     />
-                ))}
-            </div>
-            <button className="slider-button next-button" onClick={nextSlide}>
-                &#10095;
-            </button>
-            <div className="dots-container">
-                {images.map((_, index) => (
-                    <span
-                        key={index}
-                        className={`dot ${
-                            index === currentIndex ? 'active' : ''
-                        }`}
-                    ></span>
-                ))}
-            </div>
+                </div>
+            )}
         </div>
     );
 }
